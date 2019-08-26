6a74b5d33ce34f7c5ceef65410f8dab00cfb63d1
Unknown <batinsimsek1998@gmail.com>
2019-08-26 12:05:06 +0200

    register pagina

--- register.php
+++ register.php
@@ -24,3 +24,38 @@
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
 </head>
+
+<body>
+
+<main>
+  <div class="register">
+    <div class="container">
+      <div class="background-register">
+        <h2>Register Zeeslag</h2>
+        <nav class="navigatie.register">
+          <a href="index.html">Home</a>
+          <a href="login.php">Login</a>
+        </nav>
+          <form action="logincontroller.php" method="post">
+            <input type="hidden" name="type" value="register">
+
+            <label for="email"><b class="register-email">Email</b></label>
+            <input type="email" placeholder="Enter Email" name="email" id="email">
+
+            <label for="username"><b class="register-username">Username</b></label>
+            <input type="text" placeholder="Enter username" name="username">
+
+            <label for="psw"><b class="register-password">Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password">
+
+            <label for="psw"><b class="register-password-reapeat">Voer opnieuw Password in</b></label>
+            <input type="password" placeholder="Enter password again" name="passwordconfirm">
+
+            <button class="registerbutton" type="submit" value="Create new contact">Register Account</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</main>
+
+</body>

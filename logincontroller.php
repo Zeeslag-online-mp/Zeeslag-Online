93ec1fe70db40d569c19d97a38a8a43658c675b4
Unknown <batinsimsek1998@gmail.com>
2019-08-26 11:16:29 +0200

    Logincontroller

---
+++ logincontroller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gebruiker
+ * Date: 26-8-2019
+ * Time: 11:14
+ */
+
+  if($_server['REQUEST_METHODE'] != 'POST'){
+    header("index.php");
+    exit;
+  };
+
+  require 'config.php';

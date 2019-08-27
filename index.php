60ac99cb3e35a0d765deb78b490f9f76a1d6fc67
zeb <d270764@edu.rocwb.nl>
2019-08-27 16:19:13 +0200

    Added homepage + some images for website

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require 'php/header.php';?>
+
+<main class="background-img">
+	<a href="" class="button centered">NU SPELEN !</a>
+</main>
+
+<?php require 'php/footer.php';?>
---
+++ php/header.php
@@ -0,0 +1,32 @@
+<!-- Use ( require 'header.php' ) in document where you need the header -->
+
+<!DOCTYPE html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <header>
+    <div class="navbar">
+      <h1><a href="">ZEESLAG</a></h1>
+      <p><a href="">HOME</a></p>
+      <p><a href="">LOGIN</a>  OF  <a href="">MAAK EEN ACCOUNT AAN</a></p>
+    </div>   
+  </header>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->

f9a4216329a5485cdd79cc74a73c2f7538c0a35c
Unknown <Jaibreyon.lourens@gmail.com>
2019-08-29 11:45:45 +0200

    css game.php and begin app.js

--- app.js
+++ app.js
@@ -22,3 +22,26 @@ io.sockets.on('connection', function(socket){
     
 
 });
+
+
+var CONST = {};
+CONST.AVAILABLE_SHIPS = ['carrier', 'battleship', 'destroyer', 'submarine', 'patrolboat'];
+CONST.Human_Player = 0;
+CONST.Enemy_player = 1;
+//value for parameter type (string)
+CONST.CSS_TYPE_EMPTY = 'empty';
+CONST.CSS_TYPE_SHIP = 'ship';
+CONST.CSS_TYPE_MISS = 'miss';
+CONST.CSS_TYPE_HIT = 'hit';
+CONST.CSS_TYPE_SUNK = 'sunk';
+//Grid code
+CONST.TYPE_EMPTY = 0; // 0 = water (empty)
+CONST.TYPE_SHIP = 1; // 1 = undamaged ship
+CONST.TYPE_MISS = 2; // 2 = water with a cannonball in it (missed shot)
+CONST.TYPE_HIT = 3; // 3 = damaged ship (hit shot)
+CONST.TYPE_SUNK = 4; // 4 = sunk ship
+//these numbers correspomd to CONST.AVAILABLE_SHIPS
+// 0) 'carrier' 1) 'battleship' 2) 'destroyer' 3) 'submarine' 4) 'patrolboat'
+Game.usedShips = [CONST.UNUSED, CONST.UNUSED, CONST.UNUSED, CONST.UNUSED, CONST.UNUSED];
+CONST.USED = 1;
+CONST.UNUSED = 0;

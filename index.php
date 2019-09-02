bf903e8342e51c7baaca35857948d0c8d29ebab6
Unknown <Jaibreyon.lourens@gmail.com>
2019-09-02 08:44:13 +0200

    game schermen 2.0

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require 'php/header.php';?>
 
 <main class="background-img">
-	<a href="" class="button centered">NU SPELEN !</a>
+	<a href="php/" class="button centered">NU SPELEN !</a>
 </main>
 
 <?php require 'php/footer.php';?>

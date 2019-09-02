cf3fa268cd8a857da079e6662980d0c30cc32ed3
Unknown <Jaibreyon.lourens@gmail.com>
2019-09-02 08:44:23 +0200

    link naar game.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require 'php/header.php';?>
 
 <main class="background-img">
-	<a href="php/" class="button centered">NU SPELEN !</a>
+	<a href="php/game.php" class="button centered">NU SPELEN !</a>
 </main>
 
 <?php require 'php/footer.php';?>

104c00153d0ffaffbcce74e4eb5b46bacaabcb42
zeb <d270764@edu.rocwb.nl>
2019-09-03 16:31:30 +0200

    Added game
    Not our game btw

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require 'php/header.php';?>
 
 <main class="background-img">
-	<a href="php/game.php" class="button centered-home-btn">NU SPELEN !</a>
+	<a href="http://en.battleship-game.org/" class="button centered-home-btn">NU SPELEN !</a>
 </main>
 
 <?php require 'php/footer.php';?>
--- php/multiplayer.php
+++ php/multiplayer.php
@@ -2,4 +2,8 @@
 
 session_start();
 
+require 'config.php';
+
 echo $_SESSION['username'];
+
+$sql = "INSERT INTO `games` ('player_1_id', 'player_2_id', 'gamefase') VALUES (1, 2, 0);";

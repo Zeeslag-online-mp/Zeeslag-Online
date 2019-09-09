e5c6d006917baa54ebc85046461fdd1f53f8fc99
zeb <d270764@edu.rocwb.nl>
2019-09-09 10:50:31 +0200

    Fixed database file

--- database/Dump20190909.sql
+++ database/Dump20190909.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `friends`
 --
 
+CREATE DATABASE `zeeslag`;
+
+USE `zeeslag`;
+
 DROP TABLE IF EXISTS `friends`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;

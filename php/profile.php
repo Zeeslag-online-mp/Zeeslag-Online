4452470167e9b0e1280b7e4d024a4dd640f27c2a
Aboker <aboker_khalif@live.nl>
2019-09-12 11:19:58 +0200

    score en statistieken

--- php/profile.php
+++ php/profile.php
@@ -3,7 +3,9 @@
 <main>
 	<ul>
 		<li><a  style="color: #000" href=<?php echo 'http://'.$host.'/Zeeslag-Online/php/friend-list.php'?>>Vriendenlijst</a></li>
+    <li><a  style="color: #000" href=<?php echo 'http://'.$host.'/Zeeslag-Online/php/invites.php'?>>Uitnodigingen</a></li>
+    <li><a  style="color: #000" href=<?php echo 'http://'.$host.'/Zeeslag-Online/php/score.php'?>>Score</a></li>
 	</ul>
-		<li><a  style="color: #000" href=<?php echo 'http://'.$host.'/Zeeslag-Online/php/invites.php'?>>Uitnodigingen</a></li>
+
 </main>
 <?php require 'footer.php'?>

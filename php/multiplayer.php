ee0922af7da79dbc09ca914d06bbab565b82bce7
Unknown <batinsimsek1998@gmail.com>
2019-09-17 15:14:13 +0200

    aanpassing

--- php/multiplayer.php
+++ php/multiplayer.php
@@ -6,4 +6,4 @@ require 'config.php';
 
 echo $_SESSION['username'];
 
-$sql = "INSERT INTO `games` ('player_1_id', 'player_2_id', 'gamefase') VALUES (1, 2, 0);";
+$sql = "INSERT INTO `games` ('player_1_id', 'player_2_id', 'gamefase', 'ship-data-x', 'ship-data-y') VALUES (1, 2, 0);";
